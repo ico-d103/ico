01550f0a1a727623bd7dc205aa14022c39b74703
오민준 <sige_tank@naver.com>
2023-04-24 17:00:18 +0900

    feat : 프로젝트 세팅

--- ico/next.config.js
+++ ico/next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-  swcMinify: true,
-  experimental: {
-    appDir: true,
-  },
-};
+  reactStrictMode: true,
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
