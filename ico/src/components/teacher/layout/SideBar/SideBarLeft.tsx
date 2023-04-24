b7819cbd01a2fd1c68eb05489e0e0595f7f506a5
DongJu <jook1356@gmail.com>
2023-04-25 01:07:35 +0900

    feat: 사이드바 메뉴 완성

--- ico/src/components/teacher/layout/SideBar/SideBarLeft.tsx
+++ ico/src/components/teacher/layout/SideBar/SideBarLeft.tsx
@@ -1,9 +1,62 @@
 import React from 'react'
+import { css } from "@emotion/react";
 
-function SideBarLeft() {
+type SideBarLeftProps = {
+  element: {[prop: number]: {name: string, label:string, content: any}},
+  logo: any,
+  selectHandler: Function,
+  selected: number,
+}
+
+function SideBarLeft({element, logo, selectHandler, selected}: SideBarLeftProps) {
+
+  const renderElement = Object.keys(element).map((el: any, idx) => {
+    return (
+      <div css={elementWrapperCSS({target:el, selected})} onClick={() => {selectHandler(el)}}>
+        {element[el].content}
+      </div>
+    )
+  })
+
   return (
-    <div>SideBarLeft</div>
+    <div css={sideBarLeftWrapperCSS}>
+      <div css={logoWrapperCSS}>
+        {logo}
+      </div>
+      {renderElement}
+    </div>
   )
+}
+
+const sideBarLeftWrapperCSS = css`
+  width: 90px;
+  height: 100%;
+  background-color: #064F32;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  user-select: none;
+`
+
+const logoWrapperCSS = css`
+  margin-top: 36px;
+  margin-bottom: 36px;
+`
+
+const elementWrapperCSS = ({target, selected}: {target: number, selected: number}) => {
+  return css`
+    padding: 16px;
+    margin: 4px;
+    transition-property: background-color;
+    transition-duration: 0.3s;
+    border-radius: 10px;
+    background-color: ${Number(target) === Number(selected) && '#38735A'};
+    cursor: pointer;
+    
+    &:hover {
+      background-color: ${Number(target) !== Number(selected) && '#1A5B40'};
+    }
+  `
 }
 
 export default SideBarLeft
--- ico/src/components/teacher/layout/SideBar/SideBarRight.tsx
+++ ico/src/components/teacher/layout/SideBar/SideBarRight.tsx
@@ -1,9 +1,86 @@
 import React from 'react'
+import { css } from "@emotion/react";
+import { useRouter } from 'next/router';
 
-function SideBarRight() {
+type SideBarRightProps = {
+  element: {
+      name: string,
+      label: string,
+      content: any,
+      url: string
+  }[],
+  selectHandler: Function,
+  selected: number,
+  title: string,
+}
+
+function SideBarRight({element, selectHandler, selected, title}: SideBarRightProps) {
+  const router = useRouter();
+
+  const routingHandler = (value: string) => {
+    router.push(value)
+  }
+
+  const renderElement = element.map((el, idx) => {
+    return (
+      <div css={elementWrapperCSS({target:idx, selected})} onClick={() => {routingHandler(el.url); selectHandler(() => idx)}}>
+        <div css={contentWrapperCSS}>
+          {el.content}
+        </div>
+        {el.label}
+      </div>
+    )
+  })
+
   return (
-    <div>SideBarRight</div>
+    <div css={sideBarRightWrapperCSS}>
+      <div css={titleWrapperCSS}>
+        {title}
+      </div>
+      
+      {renderElement}
+    </div>
   )
 }
+
+const sideBarRightWrapperCSS = css`
+  flex: 1;
+  height: 100%;
+  background-color: #06603B;
+  display: flex;
+  flex-direction: column;
+  user-select: none;
+  padding: 40px 16px 0px 16px;
+`
+
+const titleWrapperCSS = css`
+  color: rgb(255,255,255);
+  font-size: 1.6rem;
+  margin-bottom: 44px;
+`
+
+const elementWrapperCSS = ({target, selected}: {target: number, selected: number}) => {
+  return css`
+    padding: 10px 16px 10px 16px;
+    margin: 4px;
+    transition-property: background-color;
+    transition-duration: 0.3s;
+    border-radius: 10px;
+    background-color: ${target === selected && '#38735A'};
+    cursor: pointer;
+    color: rgba(255,255,255, 0.7);
+    font-size: 1.2rem;
+    display: flex;
+    align-items: center;
+    
+    &:hover {
+      background-color: ${target !== selected && '#1E6B4B'};
+    }
+  `
+}
+
+const contentWrapperCSS = css`
+  margin-right: 12px;
+`
 
 export default SideBarRight
