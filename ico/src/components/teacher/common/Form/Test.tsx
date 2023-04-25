cb7394edad06940ec50edcd0838c07b596399515
DongJu <jook1356@gmail.com>
2023-04-25 20:41:55 +0900

    edit: 교사용 공용 폼 title 사용여부 기능 추가, prettier 적용

--- ico/src/components/teacher/common/Form/Test.tsx
+++ ico/src/components/teacher/common/Form/Test.tsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import React from "react"
 
-function Test({subInputChangeHandler}: {subInputChangeHandler?: Function}) {
-  return (
-    <div>
-        <input type="text" onChange={(event) => {subInputChangeHandler && subInputChangeHandler({key:'test', event})}}/>
-        <input type="text" onChange={(event) => {subInputChangeHandler && subInputChangeHandler({key:'test2', event})}}/>
-    </div>
-  )
+function Test({ subInputChangeHandler }: { subInputChangeHandler?: Function }) {
+	return (
+		<div>
+			<input
+				type="text"
+				onChange={(event) => {
+					subInputChangeHandler && subInputChangeHandler({ key: "test", event })
+				}}
+			/>
+			<input
+				type="text"
+				onChange={(event) => {
+					subInputChangeHandler && subInputChangeHandler({ key: "test2", event })
+				}}
+			/>
+		</div>
+	)
 }
 
 export default Test
--- ico/src/components/teacher/layout/SideBar/SideBarRight.tsx
+++ ico/src/components/teacher/layout/SideBar/SideBarRight.tsx
@@ -1,98 +1,94 @@
-import React from 'react'
-import { css } from "@emotion/react";
-import { MAIN_SIGNOUT } from './SideBarIcons';
+import React from "react"
+import { css } from "@emotion/react"
+import { MAIN_SIGNOUT } from "./SideBarIcons"
 
 type SideBarRightProps = {
-  element: {
-      name: string,
-      label: string,
-      content: any,
-      url: string
-  }[],
-  selectHandler: Function,
-  selected: number,
-  title: string,
+	element: {
+		name: string
+		label: string
+		content: any
+		url: string
+	}[]
+	selectHandler: Function
+	selected: number
+	title: string
 }
 
-function SideBarRight({element, selectHandler, selected, title}: SideBarRightProps) {
+function SideBarRight({ element, selectHandler, selected, title }: SideBarRightProps) {
+	// const signoutHandler = () => {
+	//   // 로그아웃 로직 작성
+	// }
 
-  // const signoutHandler = () => {
-  //   // 로그아웃 로직 작성
-  // }
+	const renderElement = element.map((el, idx) => {
+		return (
+			<div
+				key={`${el.name}-${idx}`}
+				css={elementWrapperCSS({ target: idx, selected })}
+				onClick={() => {
+					selectHandler(idx)
+				}}
+			>
+				<div css={contentWrapperCSS}>{el.content}</div>
+				{el.label}
+			</div>
+		)
+	})
 
-  const renderElement = element.map((el, idx) => {
-    return (
-      <div key={`${el.name}-${idx}`} css={elementWrapperCSS({target:idx, selected})} onClick={() => {selectHandler(idx)}}>
-        <div css={contentWrapperCSS}>
-          {el.content}
-        </div>
-        {el.label}
-      </div>
-    )
-  })
+	return (
+		<div css={sideBarRightWrapperCSS}>
+			<div>
+				<div css={titleWrapperCSS}>{title}</div>
+				{renderElement}
+			</div>
 
-  return (
-    <div css={sideBarRightWrapperCSS}>
-
-      <div>
-        <div css={titleWrapperCSS}>
-          {title}
-        </div>
-        {renderElement}
-      </div>
-
-      <div css={footerWrapperCSS}>
-        <div>
-          <div css={userNameCSS}>김철수 교사님</div>
-          <div css={userEmailCSS}>example@google.com</div>
-        </div>
-        
-      </div>
-
-    </div>
-  )
+			<div css={footerWrapperCSS}>
+				<div>
+					<div css={userNameCSS}>김철수 교사님</div>
+					<div css={userEmailCSS}>example@google.com</div>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 const sideBarRightWrapperCSS = css`
-  flex: 1;
-  height: 100%;
-  background-color: #06603B;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  user-select: none;
-  padding: 12% 16px 36px 16px;
+	flex: 1;
+	height: 100%;
+	background-color: #06603b;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	user-select: none;
+	padding: 12% 16px 36px 16px;
 `
 
 const titleWrapperCSS = css`
-  color: rgb(255,255,255);
-  font-size: 1.6rem;
-  margin-bottom: 46px;
+	color: rgb(255, 255, 255);
+	font-size: 1.6rem;
+	margin-bottom: 46px;
 `
 
-const elementWrapperCSS = ({target, selected}: {target: number, selected: number}) => {
-  return css`
-    padding: 11px 16px 11px 16px;
-    margin: 8px 4px 8px 4px;
-    transition-property: background-color opacity;
-    transition-duration: 0.3s;
-    border-radius: 10px;
-    background-color: ${target === selected && '#38735A'};
-    cursor: pointer;
-    color: rgba(255,255,255, 1);
-    opacity: ${target === selected ? '100%' : '60%'};
-    font-size: var(--teacher-h5);
-    font-weight: 400;
-    display: flex;
-    align-items: center;
-    
-    
-    &:hover {
-      background-color: ${target !== selected && '#38735A'};
-    }
-  `
-}
+const elementWrapperCSS = ({ target, selected }: { target: number; selected: number }) => {
+	return css`
+		padding: 11px 16px 11px 16px;
+		margin: 8px 4px 8px 4px;
+		transition-property: background-color opacity;
+		transition-duration: 0.3s;
+		border-radius: 10px;
+		background-color: ${target === selected && "#38735A"};
+		cursor: pointer;
+		color: rgba(255, 255, 255, 1);
+		opacity: ${target === selected ? "100%" : "60%"};
+		font-size: var(--teacher-h5);
+		font-weight: 400;
+		display: flex;
+		align-items: center;
 
+		&:hover {
+			background-color: ${target !== selected && "#38735A"};
+		}
+	`
+}
 
 // const footerElementWrapperCSS = ({target, selected}: {target: number, selected: number}) => {
 //   return css`
@@ -105,7 +101,7 @@ const elementWrapperCSS = ({target, selected}: {target: number, selected: number
 //     background-color: ${Number(target) === Number(selected) && '#38735A'};
 //     opacity: ${target === selected ? '100%' : '60%'};
 //     cursor: pointer;
-    
+
 //     &:hover {
 //       background-color: ${Number(target) !== Number(selected) && '#38735A'};
 //     }
@@ -113,28 +109,27 @@ const elementWrapperCSS = ({target, selected}: {target: number, selected: number
 // }
 
 const contentWrapperCSS = css`
-  width : 15%;
-  margin-right: 12px;
+	width: 15%;
+	margin-right: 12px;
 `
 
 const footerWrapperCSS = css`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
 `
 
 const userNameCSS = css`
-  color: rgb(255,255,255);
-  margin-bottom: 8px;
-  /* font-size: var(--teacher-h5); */
-  font-weight: 400;
+	color: rgb(255, 255, 255);
+	margin-bottom: 8px;
+	/* font-size: var(--teacher-h5); */
+	font-weight: 400;
 `
 
 const userEmailCSS = css`
-  color: rgba(255,255,255,0.5);
-  /* font-size: var(--teacher-h5); */
-  font-weight: 400;
+	color: rgba(255, 255, 255, 0.5);
+	/* font-size: var(--teacher-h5); */
+	font-weight: 400;
 `
-
 
 export default SideBarRight
