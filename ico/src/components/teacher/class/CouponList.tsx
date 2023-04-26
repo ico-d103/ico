833cc4b7d9481131eb61404329feb41421deb3c1
사공지은 <gonii0821@naver.com>
2023-04-26 23:26:03 +0900

    feat: 교사 쿠폰함 UI 완성

--- ico/src/components/teacher/class/CouponList.tsx
+++ ico/src/components/teacher/class/CouponList.tsx
@@ -4,11 +4,12 @@ import CouponListItem from "./CouponListItem"
 
 function CouponList() {
 	const mockList = [
-		{ id: 0, title: "노래 틀기", money: 15000, student: "4번 사공지은", date: "2023년 04월 07일" },
-		{ id: 1, title: "노래 틀기", money: 15000, student: "4번 사공지은", date: "2023년 04월 07일" },
-		{ id: 2, title: "노래 틀기", money: 15000, student: "4번 사공지은", date: "2023년 04월 07일" },
+		{ id: 0, title: "노래 틀기", money: 15000, student: "1번 강교철", date: "2023년 04월 07일" },
+		{ id: 1, title: "급식 먼저 먹기", money: 15000, student: "2번 김동주", date: "2023년 04월 07일" },
+		{ id: 2, title: "숙제 면제", money: 15000, student: "3번 변윤경", date: "2023년 04월 07일" },
 		{ id: 3, title: "노래 틀기", money: 15000, student: "4번 사공지은", date: "2023년 04월 07일" },
-		{ id: 4, title: "노래 틀기", money: 15000, student: "4번 사공지은", date: "2023년 04월 07일" },
+		{ id: 4, title: "급식 먼저 먹기", money: 15000, student: "5번 서재건", date: "2023년 04월 07일" },
+		{ id: 5, title: "숙제 면제", money: 15000, student: "6번 오민준", date: "2023년 04월 07일" },
 	]
 
 	return (
@@ -23,9 +24,10 @@ function CouponList() {
 const wrapper = css`
 	margin-top: 40px;
 	display: grid;
-	grid-template-columns: repeat(auto-fit, minmax(350px, 1fr));
+	place-items: center;
+	grid-template-columns: repeat(auto-fill, minmax(350px, 1fr));
 	grid-column-gap: 20px;
-	grid-row-gap: 30px;
+	grid-row-gap: 40px;
 `
 
 export default CouponList
