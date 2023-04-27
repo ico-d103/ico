9bb5d36b4946ca5bf7bbd04c315ae8bb74e22d66
오민준 <sige_tank@naver.com>
2023-04-27 15:21:54 +0900

    feat : 상점 공통 컴포넌트 구현

--- ico/next.config.js
+++ ico/next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+	reactStrictMode: true,
+	swcMinify: true,
+	compiler: {
+		emotion: true,
+	},
+	images: {
+		domains: ["placehold.it"],
+	},
+}
 
-  swcMinify: true,
-  compiler: {
-    emotion: true,
-  },
-};
-
-module.exports = nextConfig;
+module.exports = nextConfig
--- ico/src/components/teacher/Finance/Deposit/FinanceDepositTable.tsx
+++ ico/src/components/teacher/Finance/Deposit/FinanceDepositTable.tsx
@@ -1,7 +1,7 @@
-import TableGenerator from "../common/TableGenerator/TableGenerator"
+import TableGenerator from "../../common/TableGenerator/TableGenerator"
 import Button from "@/components/common/Button/Button"
 
-function DepositTable() {
+function FinanceDepositTable() {
 	const creditRating = [
 		["", "1등급", "2등급", "3등급", "4등급", "5등급", "6등급", "7등급", "8등급", "9등급", "10등급"],
 		["7일", "15", "12", "10", "8", "6", "5", "4", "3", "0", "0"],
@@ -26,4 +26,4 @@ function DepositTable() {
 	)
 }
 
-export default DepositTable
+export default FinanceDepositTable
