00e844fc77dedb1f440ce00ac076d7c9de2ec9c3
gyocheol <kys04171@naver.com>
2023-04-27 17:51:23 +0900

    feat : Security, JWT 설정 및 Entity 작성

--- Back-end/core-module/src/main/java/com/ico/core/entity/Student.java
+++ Back-end/core-module/src/main/java/com/ico/core/entity/Student.java
@@ -1,4 +1,38 @@
 package com.ico.core.entity;
 
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Student {
+
+    @Id
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    private Nation nation;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    private Job job;
+
+    @Column(unique = true)
+    private String identity;
+
+    private String password;
+
+    private String name;
+    private int account;
+    private boolean is_frozen;
+
+    private byte credit_score;
+    private byte number;
+    private String role;
+    private byte count;
+
 }
