8afea5deefb86f36862568ddcb1814996e6f5adc
서재건 <sjk1052005@gmail.com>
2023-04-28 15:10:31 +0900

    feat : validation-custom-exception 구현
    - 확인용 test controller, dto 생성 in api-module

--- Back-end/api-module/src/main/java/com/ico/api/controller/TestController.java
+++ Back-end/api-module/src/main/java/com/ico/api/controller/TestController.java
@@ -1,22 +1,37 @@
 package com.ico.api.controller;
 
 
+import com.ico.api.dto.TestDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
+/**
+ * Validation Test Controller
+ *
+ * @author 서재건
+ */
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/test")
 public class TestController {
 
-    @GetMapping("/")
-    public ResponseEntity<String> test() {
-        return ResponseEntity.ok().body("test OK");
+    /**
+     * Valid를 사용하여 dto 클래스에 선언한 validation 확인
+     *
+     * @param dto
+     * @return
+     */
+    @PostMapping
+    public ResponseEntity<String> test(@Valid @RequestBody TestDto dto) {
+        return ResponseEntity.ok().body("validation 통과");
     }
 
 }
