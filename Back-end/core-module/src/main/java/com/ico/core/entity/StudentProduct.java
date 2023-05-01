5c2e2af81a36fd43dbea8d57526631921484b7de
변윤경 <forever4410@gmail.com>
2023-05-01 16:07:14 +0900

    feat : 학생상품 엔티티 추가

--- Back-end/core-module/src/main/java/com/ico/core/entity/StudentProduct.java
+++ Back-end/core-module/src/main/java/com/ico/core/entity/StudentProduct.java
@@ -1,4 +1,31 @@
 package com.ico.core.entity;
 
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class StudentProduct {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "student_id")
+    private Student student;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "nation_id")
+    private Nation nation;
+    private String title;
+    private int amount;
+    private String image;
+    private String detail;
+    private byte count;
+    private boolean is_assigned;
 }
