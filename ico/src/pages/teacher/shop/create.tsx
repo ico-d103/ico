31a0f3ffe7f4e211243d7a446da74de45fa8221a
오민준 <sige_tank@naver.com>
2023-05-01 15:09:44 +0900

    feat : 상점 등록 페이지 css 일부 수정

--- ico/src/pages/teacher/shop/create.tsx
+++ ico/src/pages/teacher/shop/create.tsx
@@ -38,8 +38,8 @@ function create() {
 				</div>
 			</div>
 
-			<div css={imageCSS}>
-				<div>
+			<div>
+				<div css={imageCSS}>
 					<svg width="112" height="107" viewBox="0 0 112 107" fill="none" xmlns="http://www.w3.org/2000/svg">
 						<path
 							d="M106 53.5V69C106 80.201 106 85.8016 103.82 90.0798C101.903 93.8431 98.8431 96.9027 95.0798 98.8201C90.8016 101 85.201 101 74 101H38C26.799 101 21.1984 101 16.9202 98.8201C13.1569 96.9027 10.0973 93.8431 8.17987 90.0798C6 85.8016 6 80.201 6 69V43C6 31.799 6 26.1984 8.17987 21.9202C10.0973 18.1569 13.1569 15.0973 16.9202 13.1799C21.1984 11 26.799 11 38 11H58.5M91 36V6M76 21H106M76 56C76 67.0457 67.0457 76 56 76C44.9543 76 36 67.0457 36 56C36 44.9543 44.9543 36 56 36C67.0457 36 76 44.9543 76 56Z"
@@ -63,9 +63,7 @@ function create() {
 }
 
 const shopCreateWrapperCSS = css`
-	display: flex;
 	flex: 1;
-	flex-direction: column;
 	background-color: var(--common-back-color-2);
 	border-radius: 10px;
 	padding: 30px;
@@ -107,7 +105,7 @@ const imageCSS = css`
 
 	background-color: var(--teacher-gray2-color);
 	width: 100%;
-	height: 40%;
+	height: 350px;
 
 	margin-top: 20px;
 
