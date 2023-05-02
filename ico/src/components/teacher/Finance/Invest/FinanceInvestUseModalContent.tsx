f1371ad6d85ea3e7aa775a2e6ed8b6396f682a99
오민준 <sige_tank@naver.com>
2023-05-02 23:57:22 +0900

    feat : "투자 모달 일부 수정"

--- ico/src/components/teacher/Finance/Invest/FinanceInvestUseModalContent.tsx
+++ ico/src/components/teacher/Finance/Invest/FinanceInvestUseModalContent.tsx
@@ -5,10 +5,7 @@ import Button from "@/components/common/Button/Button"
 function FinanceInvestUseModalContent() {
 	return (
 		<>
-			<div css={contentCSS}>
-				<input type="number" placeholder="사용할 금액을 숫자만 입력해주세요." />
-				<textarea placeholder="사용 사유를 입력해주세요."></textarea>
-			</div>
+			<div>시계 자리</div>
 			<div css={buttonWrapperCSS}>
 				<Button text={"사용"} fontSize={`var(--teacher-h5)`} width={"200px"} theme={"positive"} onClick={() => {}} />
 				<Button text={"취소"} fontSize={`var(--teacher-h5)`} width={"200px"} theme={"cancelDark"} onClick={() => {}} />
@@ -16,36 +13,6 @@ function FinanceInvestUseModalContent() {
 		</>
 	)
 }
-
-const contentCSS = css`	
-	width: 100%;
-	background-color: #d5dde9;
-	border-radius: 10px;
-	padding: 15px;
-	margin-bottom: 20px;
-
-	display: flex;
-	flex-direction: column;
-
-	> input {
-		font-size: var(--teacher-h5);
-		padding: 10px;
-		background: none;
-		border: none;
-		outline: none;
-		border-bottom: 1px solid rgba(138, 108, 108, 0.1);
-	}
-
-	> textarea {
-		font-size: var(--teacher-h5);
-		padding: 10px;
-		resize: none;
-		background: none;
-		border: none;
-		outline: none;
-		height: 130px;
-	}
-`
 
 const buttonWrapperCSS = css`
 	display: flex;
