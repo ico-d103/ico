9b85a5d99a1fef493102267992c319fd112436f3
서재건 <sjk1052005@gmail.com>
2023-05-02 23:54:56 +0900

    feat : 직업 수정에서 사용할 ErrorCode 추가

--- Back-end/core-module/src/main/java/com/ico/core/exception/ErrorCode.java
+++ Back-end/core-module/src/main/java/com/ico/core/exception/ErrorCode.java
@@ -17,6 +17,9 @@ public enum ErrorCode{
 
     INVALID_FILE_EXTENSION(HttpStatus.BAD_REQUEST, "16", "파일 변환 중 에러가 발생 했습니다."),
 
+    JOB_NOT_FOUND(HttpStatus.NOT_FOUND, "15", "해당하는 직업이 없습니다."),
+
+    ALREADY_ASSIGNED(HttpStatus.BAD_REQUEST, "409", "이미 배정된 직업입니다."),
     ;
 
     private final HttpStatus httpStatus;
