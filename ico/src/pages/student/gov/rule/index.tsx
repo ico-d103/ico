42812a4f3e3f004f0cfc4183d83a191b2ea1ce48
사공지은 <gonii0821@naver.com>
2023-05-03 23:15:45 +0900

    feat: 학생 학급 규칙 UI 구현중

--- ico/src/pages/student/gov/rule/index.tsx
+++ ico/src/pages/student/gov/rule/index.tsx
@@ -1,12 +1,54 @@
+import { css } from "@emotion/react"
 import PageHeader from "@/components/student/layout/PageHeader/PageHeader"
 import GovRuleTab from "@/components/student/Gov/Rule/GovRuleTab"
+import CollapseMenu from "@/components/teacher/common/CollapseMenu/CollapseMenu"
 
 function index() {
 	return (
 		<>
 			<PageHeader title={"학급 규칙"} addComp={<GovRuleTab />} />
+			<div css={wrapperCSS}>
+				<div css={contentCSS}>
+					<CollapseMenu
+						title={"신용 등급"}
+						fontSize={`var(--student-h3)`}
+						bracketSize={"10px"}
+						children={<div>신용 등급 내용</div>}
+					/>
+					<CollapseMenu
+						title={"1. 대한민국은 민주공화국이다."}
+						fontSize={`var(--student-h3)`}
+						bracketSize={"10px"}
+						children={<div>학급 규칙 내용</div>}
+					/>
+					<CollapseMenu
+						title={"2. 대한민국의 주권은 국민에게 있고, 모든 권력은 국민으로부터 나온다."}
+						fontSize={`var(--student-h3)`}
+						bracketSize={"10px"}
+						children={<div>학급 규칙 내용</div>}
+					/>
+					<CollapseMenu
+						title={"3. 대한민국의 국민이 되는 요건은 법률로 정한다."}
+						fontSize={`var(--student-h3)`}
+						bracketSize={"10px"}
+						children={<div>학급 규칙 내용</div>}
+					/>
+				</div>
+			</div>
 		</>
 	)
 }
+
+const wrapperCSS = css`
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+`
+
+const contentCSS = css`
+	margin-top: 82px;
+	width: 95%;
+`
 
 export default index
