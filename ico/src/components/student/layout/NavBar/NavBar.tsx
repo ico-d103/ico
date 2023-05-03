832868827dffaf1ad8eae3986fbb349bac7e2443
sagongjieun <gonii0821@naver.com>
2023-05-03 16:35:28 +0900

    feat: 학생 정부 관련 페이지 생성

--- ico/src/components/student/layout/NavBar/NavBar.tsx
+++ ico/src/components/student/layout/NavBar/NavBar.tsx
@@ -13,19 +13,21 @@ type NavBarProps = {
 function NavBar({ children }: NavBarProps) {
 	const [selected, setSelected] = useState<number>(-1)
 	const router = useRouter()
-    const navigate = useNavigate()
+	const navigate = useNavigate()
 
 	const routes: { [prop: string]: number } = {
 		"/student/test": 0,
-        "/student/test2": 1,
-        "/student/test3": 2,
+		"/student/test2": 1,
+		"/student/gov/rule": 2,
+		"/student/gov/exchequer": 2,
+		"/student/gov/job": 2,
 	}
 
 	const navBarData: { [prop: number]: { url: string; name: string; label: string; content: any; function: Function } } =
 		{
 			0: { url: "/student/test", name: "home", label: "홈", content: NAVBAR_HOME, function: () => {} },
 			1: { url: "/student/test2", name: "class", label: "우리반", content: NAVBAR_CLASS, function: () => {} },
-			2: { url: "/student/test3", name: "gov", label: "정부", content: NAVBAR_GOVERNMENT, function: () => {} },
+			2: { url: "/student/gov/rule", name: "gov", label: "정부", content: NAVBAR_GOVERNMENT, function: () => {} },
 			3: { url: "/student/test", name: "store", label: "상점", content: NAVBAR_STORE, function: () => {} },
 		}
 
@@ -40,17 +42,21 @@ function NavBar({ children }: NavBarProps) {
 	const selectHandler = (value: number) => {
 		// setSelectedMain(() => value)
 		// setSelectedSub(() => 0)
-        if (value > selected) {
-            navigate(navBarData[value].url, 'rightToLeft')
-        } else {
-            navigate(navBarData[value].url, 'leftToRight')
-        }
-		
+		if (value > selected) {
+			navigate(navBarData[value].url, "rightToLeft")
+		} else {
+			navigate(navBarData[value].url, "leftToRight")
+		}
 	}
 
 	const navBarRender = Object.keys(navBarData).map((el, idx) => {
 		return (
-			<div onClick={() => {selectHandler(Number(el))}} css={navBarIndivCSS}>
+			<div
+				onClick={() => {
+					selectHandler(Number(el))
+				}}
+				css={navBarIndivCSS}
+			>
 				<div css={navBarIndivContentCSS({ targetIdx: selected, curIdx: Number(el) })}>
 					{navBarData[Number(el)].content}
 					{navBarData[Number(el)].label}
@@ -64,14 +70,11 @@ function NavBar({ children }: NavBarProps) {
 			<div css={contentWrapperCSS}>{children}</div>
 
 			<div css={navBarWrapperCSS}>
-                <div css={indicatorWrapperCSS}>
-                    <div css={indicatorCSS({length: Object.keys(navBarData).length, selected})}/>
-                </div>
-                <div css={navBarInnerWrapperCSS}>
-                    {navBarRender}
-                </div>
-                
-            </div>
+				<div css={indicatorWrapperCSS}>
+					<div css={indicatorCSS({ length: Object.keys(navBarData).length, selected })} />
+				</div>
+				<div css={navBarInnerWrapperCSS}>{navBarRender}</div>
+			</div>
 		</div>
 	)
 }
@@ -96,32 +99,29 @@ const navBarWrapperCSS = css`
 	box-shadow: 0px 0px 10px 1px rgba(0, 0, 0, 0.1);
 	position: fixed;
 	bottom: 0;
-    display: flex;
-    flex-direction: column;
-	
+	display: flex;
+	flex-direction: column;
 `
 
 const indicatorWrapperCSS = css`
-    width: 100%;
-    height: 3px;
+	width: 100%;
+	height: 3px;
 `
 
-const indicatorCSS = ({length, selected}: {length: number, selected: number}) => {
-    return css`
-        transition-property: transform;
-        transition-duration: 0.3s;
-        width: calc(100% / ${length});
-        height: 100%;
-        transform: translate(calc(${selected} * 100%), 0px);
-        background-color: rgb(70, 101, 180);
-        
-        
-    `
+const indicatorCSS = ({ length, selected }: { length: number; selected: number }) => {
+	return css`
+		transition-property: transform;
+		transition-duration: 0.3s;
+		width: calc(100% / ${length});
+		height: 100%;
+		transform: translate(calc(${selected} * 100%), 0px);
+		background-color: rgb(70, 101, 180);
+	`
 }
 
 const navBarInnerWrapperCSS = css`
-    flex: 1;
-    display: flex;
+	flex: 1;
+	display: flex;
 `
 
 const navBarIndivCSS = css`
@@ -137,14 +137,14 @@ const navBarIndivContentCSS = ({ targetIdx, curIdx }: { targetIdx: number; curId
 		flex-direction: column;
 		align-items: center;
 		& path {
-            transition-property: stroke;
-            transition-duration: 0.3s;
-			stroke: ${targetIdx === curIdx ? 'rgba(0, 0, 0, 1)' : 'rgba(0, 0, 0, 0.7)'};
+			transition-property: stroke;
+			transition-duration: 0.3s;
+			stroke: ${targetIdx === curIdx ? "rgba(0, 0, 0, 1)" : "rgba(0, 0, 0, 0.7)"};
 		}
-        transition-property: color;
-        transition-duration: 0.3s;
-        color: ${targetIdx === curIdx ? 'rgba(0, 0, 0, 1)' : 'rgba(0, 0, 0, 0.7)'};
-        font-size: 12px;
+		transition-property: color;
+		transition-duration: 0.3s;
+		color: ${targetIdx === curIdx ? "rgba(0, 0, 0, 1)" : "rgba(0, 0, 0, 0.7)"};
+		font-size: 12px;
 	`
 }
 
