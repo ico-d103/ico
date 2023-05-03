c9347ce1902dce2d4190b0dbf0f4cd33e3f7e832
변윤경 <forever4410@gmail.com>
2023-05-03 16:33:22 +0900

    edit : 예외처리 함수 수정 - nation 객체로 찾는 것에서 nation_id로 찾는걸로 변경

--- Back-end/core-module/src/main/java/com/ico/core/repository/TeacherProductRepository.java
+++ Back-end/core-module/src/main/java/com/ico/core/repository/TeacherProductRepository.java
@@ -1,9 +1,8 @@
 package com.ico.core.repository;
 
-import com.ico.core.entity.Nation;
 import com.ico.core.entity.TeacherProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TeacherProductRepository extends JpaRepository<TeacherProduct, Long> {
-    boolean existsByNationAndTitle(Nation nation, String title);
+    boolean existsByNationIdAndTitle(Long nationId, String title);
 }
