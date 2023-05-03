278627d3172b832e36ff08227a5ff090ee6298d2
강교철 <kys04171@naver.com>
2023-05-03 19:37:05 +0900

    edit : 로그인 URI에 /api 추가

--- Back-end/api-module/src/main/java/com/ico/api/controller/MemberController.java
+++ Back-end/api-module/src/main/java/com/ico/api/controller/MemberController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api")
 public class MemberController {
 
     private final MemberService memberService;
