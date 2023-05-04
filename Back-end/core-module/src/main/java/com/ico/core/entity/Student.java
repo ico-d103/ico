2ea15a5732645120f15111dd2b4cdae248c3abff
서재건 <sjk1052005@gmail.com>
2023-05-04 15:24:20 +0900

    edit : 학생 신용 등급 컬럼 추가 및 컬럼 순서 수정

--- Back-end/core-module/src/main/java/com/ico/core/entity/Student.java
+++ Back-end/core-module/src/main/java/com/ico/core/entity/Student.java
@@ -22,6 +22,7 @@ import javax.persistence.OneToOne;
 
 /**
  * @author 강교철
+ * @author 서재건
  */
 @Entity
 @Builder
@@ -34,22 +35,35 @@ public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @OneToOne(fetch = FetchType.LAZY)
     private Nation nation;
+
     @OneToOne(fetch = FetchType.LAZY)
     private Job job;
+
     @Column(nullable = false, unique = true)
     private String identity;
-    @Column(nullable = false)
-    private String password;
+
     private String name;
-    private int account;
-    private boolean isFrozen;
-    private byte creditScore;
+
     private byte number;
+
+    private int account;
+
+    private boolean isFrozen;
+
+    private short creditScore;
+
+    private byte creditRating;
+
+    private int salary;
+
     @Enumerated(EnumType.STRING)
     private Role role;
-    private int salary;
+
+    @Column(nullable = false)
+    private String password;
 
     public void encodeStudentPassword(PasswordEncoder passwordEncoder){
         this.password = passwordEncoder.encode(password);
