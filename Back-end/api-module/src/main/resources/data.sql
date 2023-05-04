6268bf77369f27dc634f3b6016d6e6df3cb0e84b
서재건 <sjk1052005@gmail.com>
2023-05-04 17:37:16 +0900

    edit : data.sql 데이터 추가 쿼리문 수정
    - 학생과 나라 추가 쿼리문 확인 필요

--- Back-end/api-module/src/main/resources/data.sql
+++ Back-end/api-module/src/main/resources/data.sql
@@ -1,5 +1,5 @@
 insert into nation(id, code, credit_down, credit_up, currency, grade, room, school, stock, title, trading_end, trading_start, treasury)
-VALUES (1, 'code', 0, 0, '미소', 6, 1, '싸피 초등학교', '몸무게', '활명수국', '11:22:59', '11:22:59', 0);
+VALUES (99, 'code', 0, 0, '미소', 6, 1, '싸피 초등학교', '몸무게', '활명수국', '11:22:59', '11:22:59', 0);
 
 insert into job(id, color, credit_rating, detail, image, title, total, wage, nation_id)
 VALUES(1, '빨강', 5, '이 분은 소방관', '일러스트...', '소방관', 5, 1000, 1);
@@ -7,6 +7,6 @@ VALUES(1, '빨강', 5, '이 분은 소방관', '일러스트...', '소방관', 5
 insert into job(id, color, credit_rating, detail, image, title, total, wage, nation_id)
 VALUES(2, '파랑', 5, '이 분은 경찰관', '일러스트...', '경찰관', 3, 1500, 1);
 
-insert into student(id, nation_id, job_id, identity, password, name, account, is_frozen, student.credit_score, number, role, salary)
-values (0, 1, 1, 'test', 'kdljfiowejflksdg', 'everyone', 10000, 0, 5, 1, 'STUDENT', 1);
+insert into student(id, nation_id, job_id, identity, password, name, account, is_frozen, student.credit_score, credit_rating, number, role, salary)
+values (99, 99, 0, 'test', 'kdljfiowejflksdg', 'everyone', 10000, 0, 5, 5, 1, 'STUDENT', 1);
 
