b02b6f3e6245026d21631311353063dc60983803
서재건 <sjk1052005@gmail.com>
2023-05-04 11:22:25 +0900

    feat : 학생 더미 데이터 추가

--- Back-end/api-module/src/main/resources/data.sql
+++ Back-end/api-module/src/main/resources/data.sql
@@ -7,3 +7,6 @@ VALUES(1, '빨강', 5, '이 분은 소방관', '일러스트...', '소방관', 5
 insert into job(id, color, credit_rating, detail, image, title, total, wage, nation_id)
 VALUES(2, '파랑', 5, '이 분은 경찰관', '일러스트...', '경찰관', 3, 1500, 1);
 
+insert into student(id, nation_id, job_id, identity, password, name, account, is_frozen, student.credit_score, number, role, salary)
+values (0, 1, 1, 'test', 'kdljfiowejflksdg', 'everyone', 10000, 0, 5, 1, 'STUDENT', 1);
+
