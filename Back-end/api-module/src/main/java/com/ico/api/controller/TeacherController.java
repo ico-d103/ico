69c15ea4d837e8b083102e08db535e1169708fb9
변윤경 <forever4410@gmail.com>
2023-05-04 15:31:36 +0900

    docs : @author 줄바꿈

--- Back-end/api-module/src/main/java/com/ico/api/controller/TeacherController.java
+++ Back-end/api-module/src/main/java/com/ico/api/controller/TeacherController.java
@@ -1,23 +1,21 @@
 package com.ico.api.controller;
 
-import com.ico.api.dto.AccountDto;
 import com.ico.api.dto.TeacherSignUpRequestDto;
 import com.ico.api.service.TeacherService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
 
 /**
  * Teacher Controller
  *
- * @author 강교철, 변윤경
+ * @author 강교철
+ * @author 변윤경
  */
 @RestController
 @RequiredArgsConstructor
