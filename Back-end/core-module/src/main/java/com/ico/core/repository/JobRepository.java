30ccd98d5b9d2d780f3996032db69eee0d14fbf4
서재건 <sjk1052005@gmail.com>
2023-05-04 14:05:23 +0900

    docs : javadoc 추가

--- Back-end/core-module/src/main/java/com/ico/core/repository/JobRepository.java
+++ Back-end/core-module/src/main/java/com/ico/core/repository/JobRepository.java
@@ -4,8 +4,29 @@ import com.ico.core.entity.Job;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
+/**
+ * 직업 관련 Repository
+ *
+ * @author 서재건
+ */
 public interface JobRepository extends JpaRepository<Job, Long> {
 
+    /**
+     * 학급의 모든 직업 조히
+     *
+     * @param nationId
+     * @return
+     */
     List<Job> findAllByNationId(Long nationId);
+
+    /**
+     * 학급의 특정 직업 조회
+     *
+     * @param id
+     * @param nationId
+     * @return
+     */
+    Optional<Job> findByIdAndNationId(Long id, Long nationId);
 }
