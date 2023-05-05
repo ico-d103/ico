c3868c979a99fbcef9ead4fa8ca9ed3dd2d44481
사공지은 <gonii0821@naver.com>
2023-05-06 00:24:25 +0900

    feat: 학생 직업 목록 UI 구현

---
+++ ico/src/components/student/Gov/Job/GovJobCard.tsx
@@ -0,0 +1,105 @@
+import LoadImage from "@/components/common/LoadImage/LoadImage"
+import { css } from "@emotion/react"
+
+type GovJobCardPropsType = {
+	mock: {
+		id: number
+		student: string
+		name: string
+		content: string
+		grade: number
+		money: number
+	}
+}
+
+function GovJobCard({ mock }: GovJobCardPropsType) {
+	return (
+		<div css={cardWrapperCSS}>
+			<div css={imageWrapperCSS}>
+				<LoadImage wrapperCss={imgCSS} src={"/assets/job/weather_caster.png"} alt={"job_image"} />
+			</div>
+			<div css={contentWrapperCSS}>
+				<h3>{mock.student}</h3>
+				<h4>{mock.name}</h4>
+				<div css={divideCSS}></div>
+				<div css={conditionWrapperCSS}>
+					<h4>신용 등급</h4>
+					<h3>{mock.grade}등급 이상</h3>
+				</div>
+				<div css={conditionWrapperCSS}>
+					<h4>월급</h4>
+					<h3>{mock.money} 미소</h3>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+const cardWrapperCSS = css`
+	width: 270px;
+	min-width: 270px;
+	height: 150px;
+	border-radius: 10px;
+	background: #007bc0;
+	position: relative;
+	transition: all 0.2s;
+`
+
+const imageWrapperCSS = css`
+	position: absolute;
+	top: -20px;
+	left: 10px;
+	width: 102px;
+	height: 164px;
+`
+
+const imgCSS = css`
+	width: 100%;
+	height: 100%;
+
+	filter: drop-shadow(0px 0px 10px rgba(0, 0, 0, 0.2));
+`
+
+const contentWrapperCSS = css`
+	position: absolute;
+	width: 130px;
+	top: 20px;
+	left: 120px;
+
+	> h3 {
+		color: var(--common-back-color-2);
+		font-size: var(--student-h3);
+		margin-bottom: 7px;
+	}
+
+	> h4 {
+		font-size: var(--student-h4);
+		color: rgba(255, 255, 255, 0.6);
+		line-height: 18px;
+	}
+`
+
+const divideCSS = css`
+	margin: 10px 0;
+	border: 1px solid rgba(255, 255, 255, 0.4);
+`
+
+const conditionWrapperCSS = css`
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	align-items: center;
+	margin-bottom: 7px;
+
+	> h3 {
+		font-size: var(--student-h3);
+		color: var(--common-back-color-2);
+	}
+
+	> h4 {
+		font-size: var(--student-h4);
+		color: rgba(255, 255, 255, 0.6);
+	}
+`
+
+export default GovJobCard
--- ico/src/pages/student/gov/job/index.tsx
+++ ico/src/pages/student/gov/job/index.tsx
@@ -1,17 +1,50 @@
 import { css } from "@emotion/react"
 import GovRuleTab from "@/components/student/Gov/Rule/GovRuleTab"
 import PageHeader from "@/components/student/layout/PageHeader/PageHeader"
+import GovJobCard from "@/components/student/Gov/Job/GovJobCard"
 
 function index() {
+	const mockList = [
+		{
+			id: 0,
+			student: "사공지은",
+			name: "기상 캐스터",
+			content: "학생들의 의견을 수렴하여 에어컨/히터의 온도를 조절하고 다음날 날씨를 알려주는 직업",
+			grade: 6,
+			money: 17000,
+		},
+		{
+			id: 1,
+			student: "담당자 미정",
+			name: "소방관",
+			content: "교실 내 소화기를 주기적으로 관리하는 직업",
+			grade: 3,
+			money: 20000,
+		},
+		{
+			id: 2,
+			student: "사공지은",
+			name: "기상 캐스터",
+			content: "학생들의 의견을 수렴하여 에어컨/히터의 온도를 조절하고 다음날 날씨를 알려주는 직업",
+			grade: 6,
+			money: 17000,
+		},
+	]
+
 	return (
 		<>
 			<PageHeader title={"직업 목록"} addComp={<GovRuleTab />} />
 			<div css={wrapperCSS}>
 				<div css={contentCSS}>
-					<div css={titleCSS}>
-						우리반에는 <span>20개</span>의 직업이 있어요
+					<span css={titleCSS}>
+						우리반에는
+						<b>&nbsp;20개</b>의 직업이 있어요
+					</span>
+					<div css={jobListCSS}>
+						{mockList.map((mock) => (
+							<GovJobCard key={mock.id} mock={mock} />
+						))}
 					</div>
-					<div css={jobListCSS}></div>
 				</div>
 			</div>
 		</>
@@ -35,7 +68,7 @@ const titleCSS = css`
 	align-items: center;
 	font-size: 1.1rem;
 
-	> span {
+	> b {
 		font-weight: bold;
 		color: var(--student-main-color-5);
 	}
@@ -47,6 +80,9 @@ const jobListCSS = css`
 	width: 100%;
 	background-color: var(--student-wrapper-color);
 	border-radius: 10px;
+	display: flex;
+	flex-direction: column;
+	gap: 30px;
 `
 
 export default index
