a3a582f106ec1609615e884ec2be0a86313bd00a
사공지은 <gonii0821@naver.com>
2023-05-07 00:14:22 +0900

    feat: 학생 구인구직 카드뷰 구현

---
+++ ico/src/components/student/Class/JobSearch/ClassJobSearchCard.tsx
@@ -0,0 +1,75 @@
+import LoadImage from "@/components/common/LoadImage/LoadImage"
+import { css } from "@emotion/react"
+
+type ClassJobSearchCardPropsType = {
+	mock: {
+		id: number
+		name: string
+		need: number
+	}
+}
+
+function ClassJobSearchCard({ mock }: ClassJobSearchCardPropsType) {
+	return (
+		<div css={wrapperCSS}>
+			<div css={imageWrapperCSS}>
+				<LoadImage wrapperCss={imgCSS} src={"/assets/job/weather_caster.png"} alt={"job_image"} />
+			</div>
+			<div css={contentWrapperCSS}>
+				<span css={nameCSS}>{mock.name}</span>
+				<span css={needCSS}>{mock.need}</span>
+			</div>
+		</div>
+	)
+}
+
+const wrapperCSS = css`
+	width: 150px;
+	height: 100px;
+	background-color: #007bc0;
+	box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+	border-radius: 10px;
+	position: relative;
+`
+
+const imageWrapperCSS = css`
+	position: absolute;
+	top: 5px;
+	left: 10px;
+	width: 50px;
+	height: 90px;
+`
+
+const imgCSS = css`
+	width: 100%;
+	height: 100%;
+
+	filter: drop-shadow(0px 0px 10px rgba(0, 0, 0, 0.2));
+`
+
+const contentWrapperCSS = css`
+	width: 70px;
+	height: 80px;
+	position: absolute;
+	top: 10px;
+	right: 10px;
+
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+`
+
+const nameCSS = css`
+	text-align: right;
+	font-size: var(--student-h3);
+	color: white;
+`
+
+const needCSS = css`
+	text-align: right;
+	color: rgba(255, 255, 255, 0.7);
+	font-size: 3rem;
+	font-weight: bold;
+`
+
+export default ClassJobSearchCard
--- ico/src/pages/student/class/jobsearch.tsx
+++ ico/src/pages/student/class/jobsearch.tsx
@@ -1,7 +1,17 @@
 import PageHeader from "@/components/student/layout/PageHeader/PageHeader"
 import { css } from "@emotion/react"
+import ClassJobSearchCard from "@/components/student/Class/JobSearch/ClassJobSearchCard"
+import useNavigate from "@/hooks/useNavigate"
 
 function jobsearch() {
+	const navigate = useNavigate()
+
+	const mockList = [
+		{ id: 0, name: "기상캐스터", need: 1 },
+		{ id: 1, name: "소방관", need: 2 },
+		{ id: 2, name: "한국 전력", need: 3 },
+	]
+
 	return (
 		<>
 			<PageHeader title={"일자리 찾기"} />
@@ -10,6 +20,19 @@ function jobsearch() {
 					<span css={titleCSS}>
 						현재 <b>&nbsp;5개</b>의 직업을 구인하고 있어요
 					</span>
+					<div css={jobListCSS}>
+						{mockList.map((mock) => (
+							<ClassJobSearchCard key={mock.id} mock={mock} />
+						))}
+					</div>
+					<div
+						css={floatingWrapperCSS}
+						onClick={() => {
+							navigate("/student/gov/job")
+						}}
+					>
+						<div css={floatingCSS}>직업 설명 자세히 볼래요</div>
+					</div>
 				</div>
 			</div>
 		</>
@@ -37,6 +60,35 @@ const titleCSS = css`
 		font-weight: bold;
 		color: var(--student-main-color-5);
 	}
+`
+
+const jobListCSS = css`
+	margin-top: 30px;
+	padding: 30px 20px;
+	width: 100%;
+	background-color: var(--student-wrapper-color);
+	border-radius: 10px;
+
+	display: grid;
+	place-items: center;
+	grid-template-columns: repeat(auto-fill, minmax(145px, 1fr));
+	grid-column-gap: 10px;
+	grid-row-gap: 20px;
+`
+
+const floatingWrapperCSS = css`
+	display: flex;
+	justify-content: center;
+`
+
+const floatingCSS = css`
+	background: rgba(0, 102, 255, 0.2);
+	padding: 10px 20px;
+	border-radius: 20px;
+	position: fixed;
+	bottom: 90px;
+	background-color: var(--student-main-color-3);
+	color: var(--student-font-color);
 `
 
 export default jobsearch
