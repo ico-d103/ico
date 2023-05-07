49bc99bbc05090f0240f8de4097499fe681b04bb
서재건 <sjk1052005@gmail.com>
2023-05-07 14:50:15 +0900

    feat : 학급규칙 추가 구현

--- Back-end/api-module/src/main/java/com/ico/api/controller/RuleController.java
+++ Back-end/api-module/src/main/java/com/ico/api/controller/RuleController.java
@@ -1,15 +1,20 @@
 package com.ico.api.controller;
 
 
+import com.ico.api.dto.rule.RuleReqDto;
 import com.ico.api.dto.rule.RuleResDto;
 import com.ico.api.service.rule.RuleService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.util.List;
 
 /**
@@ -33,5 +38,17 @@ public class RuleController {
     @GetMapping
     public ResponseEntity<List<RuleResDto>> findAllRule() {
         return ResponseEntity.ok(ruleService.findAllRule());
+    }
+
+    /**
+     * 학급규칙 추가
+     *
+     * @param dto
+     * @return
+     */
+    @PostMapping("/teacher")
+    public ResponseEntity<HttpStatus> addRule(@Valid @RequestBody RuleReqDto dto) {
+        ruleService.addRule(dto);
+        return ResponseEntity.ok(HttpStatus.OK);
     }
 }
