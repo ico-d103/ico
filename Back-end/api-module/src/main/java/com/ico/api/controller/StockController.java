c0d1a49d6199882cdbcc9659b362eb80da06c898
변윤경 <forever4410@gmail.com>
2023-05-07 16:54:20 +0900

    docs : 교사의 투자 이슈 조회

--- Back-end/api-module/src/main/java/com/ico/api/controller/StockController.java
+++ Back-end/api-module/src/main/java/com/ico/api/controller/StockController.java
@@ -18,6 +18,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class StockController {
     private final StockService stockService;
 
+    /**
+     * 교사의 투자 이슈 조회
+     * @return 투자 이슈 정보, 투자 종목 정보(거래가능시간, 이름)
+     */
     @GetMapping("/teacher")
     public ResponseEntity<StockResTeacherDto> stockIssueTeacher(){
         return ResponseEntity.ok(stockService.getIssueTeacher());
