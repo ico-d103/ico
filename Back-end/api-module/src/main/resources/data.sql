e69f214103d36ae9180d13da30adec4554a8d3af
서재건 <sjk1052005@gmail.com>
2023-05-08 15:52:28 +0900

    edit : data.sql 더미 데이터 insert query문 삭제

--- Back-end/api-module/src/main/resources/data.sql
+++ Back-end/api-module/src/main/resources/data.sql
@@ -7,11 +7,4 @@ VALUES(1, '빨강', 5, 0, '이 분은 소방관', '일러스트...', '소방관'
 insert into job(id, color, credit_rating, detail, image, title, total, wage, nation_id)
 VALUES(2, '파랑', 5, '이 분은 경찰관', '일러스트...', '경찰관', 3, 1500, 99);
 
-insert into student(account, credit_rating, credit_score, identity, is_frozen, name, number, password, role, salary, nation_id)
-values (10000, 5, 500, 'testid', false, 'test', 31, 'efasdvcxzcvads', 'STUDENT', 10000, 99);
-
-insert into rule(id, title, detail, nation_id) VALUES(97, '우리', '행복하자', 99);
-insert into rule(id, detail, title, nation_id) VALUES(98, '다들', '성공하자', 99);
-insert into rule(id, detail, title, nation_id) VALUES(99, '나는', '할 수 있다!', 99);
-insert into rule(id, detail, title, nation_id) VALUES(100, 'I AM', 'GROUND', 99);
 
