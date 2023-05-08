7c09d88f7245930536a3821fcca3cf970f812ee3
서재건 <sjk1052005@gmail.com>
2023-05-08 23:14:55 +0900

    feat : 직업 명함 조회 구현 및 간단 리팩토링
    - 수정 시 총 인원 수가 배정 인원보다 적어지는 경우 예외 처리

--- Back-end/core-module/src/main/java/com/ico/core/exception/ErrorCode.java
+++ Back-end/core-module/src/main/java/com/ico/core/exception/ErrorCode.java
@@ -71,6 +71,8 @@ public enum ErrorCode{
 
     RULE_NOT_FOUND(HttpStatus.NOT_FOUND, "423", "해당하는 규칙이 없습니다."),
 
+    INVALID_JOB_TOTAL(HttpStatus.BAD_REQUEST, "424", "배정된 인원보다 적은 총 인원 수를 설정할 수 없습니다."),
+
     INVALID_CREDIT_RATING(HttpStatus.BAD_REQUEST, "500", "신용등급 요건에 적합하지 않습니다."),
 
     REQUEST_NOT_FOUND(HttpStatus.NOT_FOUND, "503", "신청 내역이 존재하지 않습니다."),
