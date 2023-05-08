c0851b249c57080e852d4aeb3d8f385c157d61f0
sagongjieun <gonii0821@naver.com>
2023-05-08 14:13:47 +0900

    feat: 공통 아이디 중복 체크 기능 구현

--- ico/src/api/common/postDuplicationCheck.ts
+++ ico/src/api/common/postDuplicationCheck.ts
@@ -2,16 +2,21 @@ import { defaultInstance } from "@/api/instance"
 import { postDuplicationCheckType } from "@/types/common/apiReturnTypes"
 
 type bodyType = {
-	id: string
+	identity: string
 }
 
-type responseType = postDuplicationCheckType
+type responseType = {
+	status: number
+	data: postDuplicationCheckType
+}
 
 export const postDuplicationCheck = async (body: bodyType) => {
 	try {
+		console.log(body)
 		const response: responseType = await defaultInstance.post("/duplicated-id", body)
+		console.log(response)
 
-		return response.duplication
+		return response.data
 	} catch (error) {
 		console.log(error)
 	}
