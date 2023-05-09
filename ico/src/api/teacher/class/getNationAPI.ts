2f7d0c50a19910c40ccf53829437b22205bd5369
sagongjieun <gonii0821@naver.com>
2023-05-09 13:11:00 +0900

    feat: 교사 로그인 후 분기처리 구현

--- ico/src/api/teacher/class/getNationAPI.ts
+++ ico/src/api/teacher/class/getNationAPI.ts
@@ -9,11 +9,9 @@ type responseType = {
 export const getNationAPI = async () => {
 	try {
 		const response: responseType = await tokenInstance.get("/nation/teacher")
-		console.log("교사가 생성한 나라 요청 response : ", response)
 
-		// if (response.status === 200) return response.data
+		return response.data
 	} catch (error) {
-		console.log("@@@", error)
 		throw error
 	}
 }
