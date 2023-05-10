5848f39d3766d45774cccf9c2280ae109849f590
sagongjieun <gonii0821@naver.com>
2023-05-10 13:29:45 +0900

    feat: 선택된 학생 없을 시 상세 보기 화면 구현

--- ico/src/components/teacher/Class/Student/Detail/ClassStudentDetail.tsx
+++ ico/src/components/teacher/Class/Student/Detail/ClassStudentDetail.tsx
@@ -12,14 +12,34 @@ function StudentDetail() {
 
 	return (
 		<>
-			<h1 css={headerCSS}>학생 정보 상세보기</h1>
-			<ClassStudentDetailHead />
-			<ClassStudentDetailMoney />
-			<ClassStudentDetailGrade />
-			<ClassStudentDetailAccountList />
+			{selectedStudentAtom === -1 ? (
+				<div css={wrapperCSS}>
+					<h1>관리할 학생을 선택해주세요.</h1>
+				</div>
+			) : (
+				<>
+					<h1 css={headerCSS}>학생 정보 상세보기</h1>
+					<ClassStudentDetailHead />
+					<ClassStudentDetailMoney />
+					<ClassStudentDetailGrade />
+					<ClassStudentDetailAccountList />
+				</>
+			)}
 		</>
 	)
 }
+
+const wrapperCSS = css`
+	height: 100%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+
+	> h1 {
+		font-size: var(--teacher-h2);
+		color: var(--teacher-gray-color);
+	}
+`
 
 const headerCSS = css`
 	font-size: var(--teacher-h1);
