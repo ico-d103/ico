2e07028716cc6d4ef2c89de04773727af935b318
DongJu <jook1356@gmail.com>
2023-05-11 01:18:06 +0900

    feat: 화면 전환시 블러효과 추가

--- ico/src/components/student/Home/Asset/HomeAssetItem.tsx
+++ ico/src/components/student/Home/Asset/HomeAssetItem.tsx
@@ -1,93 +1,95 @@
-import React, {useState} from 'react'
+import React, { useState } from "react"
 import { css } from "@emotion/react"
-import Button from '@/components/common/Button/Button'
-import useNavigate from '@/hooks/useNavigate'
-import Link from 'next/link'
+import Button from "@/components/common/Button/Button"
+import useNavigate from "@/hooks/useNavigate"
+import Link from "next/link"
 
 type HomeAssetItemProps = {
-    icon: any
-    title: string
-    money: number
-    moneyUnit: string
-    detailUrl: string
-
-
+	icon: any
+	title: string
+	money: number
+	moneyUnit: string
+	detailUrl: string
 }
 
-function HomeAssetItem({icon, title, money, moneyUnit, detailUrl }: HomeAssetItemProps) {
+function HomeAssetItem({ icon, title, money, moneyUnit, detailUrl }: HomeAssetItemProps) {
+	const navigate = useNavigate()
 
-    const navigate = useNavigate()
-    
-  return (
-    <div css={contentWrapperCSS}>
-        <Link  href={detailUrl} prefetch/>
-        <div css={leftWrapperCSS}>
-            <div css={imgWrapperCSS}>
-                {icon}
-            </div>
-            <div css={textContentWrapperCSS}>
-                <div css={titleWrapperCSS}>
-                    {title}
-                </div>
-                <div css={moneyWrapperCSS}>
-                    {money.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")}{moneyUnit}
-                </div>  
-            </div>
-        </div>
-        <div>
-            <Button text={"자세히"} fontSize={`var(--teacher-h5)`} width={"72px"} height={'100%'} theme={"mobileNormal"} onClick={() => {navigate(detailUrl, 'bottomToTop')}} />
-            
-        </div>
-    </div>
-  )
+	return (
+		<React.Fragment>
+			<Link href={detailUrl} prefetch />
+			<div css={contentWrapperCSS}>
+				<div css={leftWrapperCSS}>
+					<div css={imgWrapperCSS}>{icon}</div>
+					<div css={textContentWrapperCSS}>
+						<div css={titleWrapperCSS}>{title}</div>
+						<div css={moneyWrapperCSS}>
+							{money.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")}
+							{moneyUnit}
+						</div>
+					</div>
+				</div>
+				<div>
+					<Button
+						text={"자세히"}
+						fontSize={`var(--teacher-h5)`}
+						width={"72px"}
+						height={"100%"}
+						theme={"mobileNormal"}
+						onClick={() => {
+							navigate(detailUrl, "bottomToTop")
+						}}
+					/>
+				</div>
+			</div>
+		</React.Fragment>
+	)
 }
 
 const contentWrapperCSS = css`
-    display: flex;
-    justify-content: space-between;
-    width: 100%;
-    margin: 20px 0px;
+	display: flex;
+	justify-content: space-between;
+	width: 100%;
+	margin: 20px 0px;
 `
 
 const leftWrapperCSS = css`
-    display: flex;
+	display: flex;
 `
 
 const imgWrapperCSS = css`
-    border-radius: 9999px;
-    background-color: #f7dd9d;
-    /* border: 0.5px solid #feb600; */
-    width: 36px;
-    height: 36px;
-    margin-right: 12px;
+	border-radius: 9999px;
+	background-color: #f7dd9d;
+	/* border: 0.5px solid #feb600; */
+	width: 36px;
+	height: 36px;
+	margin-right: 12px;
 
-    display: flex;
-    justify-content:center;
-    align-items: center;
+	display: flex;
+	justify-content: center;
+	align-items: center;
 
-    & path {
-        stroke: #9b6f00;
-        
-    }
+	& path {
+		stroke: #9b6f00;
+	}
 `
 
 const textContentWrapperCSS = css`
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-
+	height: 100%;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
 `
 
 const titleWrapperCSS = css`
-    font-size: var(--student-h4);
-    color: rgba(0, 0, 0, 0.6);
-    font-weight: 500;
+	font-size: var(--student-h4);
+	color: rgba(0, 0, 0, 0.6);
+	font-weight: 500;
 `
 
 const moneyWrapperCSS = css`
-    font-size: 1.1rem;
-    font-weight: 700;
+	font-size: 1.1rem;
+	font-weight: 700;
 `
 
 export default HomeAssetItem
