bf738de2cc9177a0d8867ada65f4c288b35b9f3a
변윤경 <forever4410@gmail.com>
2023-05-11 23:53:36 +0900

    edit : studentProduct request 추가

--- Back-end/api-module/src/main/java/com/ico/api/service/student/StudentProductService.java
+++ Back-end/api-module/src/main/java/com/ico/api/service/student/StudentProductService.java
@@ -5,6 +5,7 @@ import com.ico.api.dto.studentProduct.StudentProductDetailResDto;
 import com.ico.api.dto.studentProduct.StudentProductReqDto;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 /**
@@ -16,27 +17,27 @@ public interface StudentProductService {
      * 학생의 상품 판매 제안서를 학생 상품 테이블에 추가합니다.
      * @param proposal 판매제안서 양식
      */
-    void createProduct(List<MultipartFile> files, StudentProductReqDto proposal);
+    void createProduct(HttpServletRequest request, List<MultipartFile> files, StudentProductReqDto proposal);
 
     /**
      * 등록된 학생 상품 목록을 조회합니다.
      * @return 학생 상품 목록
      */
-    List<StudentProductAllResDto> findAllProduct();
+    List<StudentProductAllResDto> findAllProduct(HttpServletRequest request);
 
     /**
      * 학생의 판매 제안서 승인
      *
      * @param id 학생 상품 id
      */
-    void updateIsAssigned(Long id);
+    void updateIsAssigned(HttpServletRequest request, Long id);
 
     /**
      * 학생 상품 삭제
      *
      * @param id 학생 상품 id
      */
-    void deleteProduct(Long id);
+    void deleteProduct(HttpServletRequest request, Long id);
 
     /**
      * 학생상품 상세보기
@@ -44,5 +45,5 @@ public interface StudentProductService {
      * @param id 상품 아이디
      * @return 상품디테일 정보
      */
-    StudentProductDetailResDto detailProduct(Long id);
+    StudentProductDetailResDto detailProduct(HttpServletRequest request, Long id);
 }
