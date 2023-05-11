005bd87e80e9d46911953e2c714ff3874be8f259
강교철 <kys04171@naver.com>
2023-05-11 18:02:11 +0900

    feat : 리다이렉트 개발중

--- Back-end/api-module/src/main/java/com/ico/api/user/JwtTokenAuthenticationFilter.java
+++ Back-end/api-module/src/main/java/com/ico/api/user/JwtTokenAuthenticationFilter.java
@@ -1,5 +1,7 @@
 package com.ico.api.user;
 
+import com.ico.core.code.Role;
+import com.ico.core.entity.Student;
 import com.ico.core.exception.CustomException;
 import com.ico.core.exception.ErrorCode;
 import io.jsonwebtoken.ExpiredJwtException;
@@ -31,35 +33,86 @@ public class JwtTokenAuthenticationFilter extends OncePerRequestFilter {
     private final JwtTokenProvider jwtTokenProvider;
     private final CustomUserDetailService customUserDetailService;
 
+//    @Override
+//    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+//                                    FilterChain filterChain) throws ServletException, IOException {
+//        String token = "";
+//        try {
+//            // HttpServletRequest 객체에서 JWT 토큰을 추출
+//            token = jwtTokenProvider.parseJwt(request);
+//            log.info("request: {}", request.getHeader("Authorization"));
+//            log.info("token: {}", token);
+//
+//            // 추출된 JWT 토큰이 null이 아닌 경우, 해당 토큰에서 identity 값을 가져오기
+//            if (token != null) {
+//                String identity = jwtTokenProvider.getIdentity(token);
+//                log.info("identity: {}", identity);
+//                CustomUserDetails userDetails = customUserDetailService.loadUserByUsername(identity);
+//                log.info("userDetail.getAuthorities: {}", userDetails.getAuthorities());
+//
+//                // 가져온 사용자 정보를 사용하여 UsernamePasswordAuthenticationToken 객체를 생성하고, SecurityContext에 이를 설정
+//                Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+//                SecurityContext context = SecurityContextHolder.getContext();
+//                context.setAuthentication(authentication);
+//                SecurityContextHolder.setContext(context);
+//                log.info("SecurityContextHolder 저장 완료");
+//            }
+//        } catch (ExpiredJwtException e){
+//            log.info("[doFilterInternal]에서 발생 : {}", e.getMessage());
+//            e.printStackTrace();
+//        }
+//        // HTTP 요청을 필터링한 후 다음 필터로 체인을 전달
+//        filterChain.doFilter(request, response);
+//    }
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
-        String token = "";
-        try {
-            // HttpServletRequest 객체에서 JWT 토큰을 추출
-            token = jwtTokenProvider.parseJwt(request);
-            log.info("request: {}", request.getHeader("Authorization"));
-            log.info("token: {}", token);
+        String token = jwtTokenProvider.parseJwt(request);
+        if (token != null) {
+            Role role = jwtTokenProvider.getRole(token);
+            String identity = jwtTokenProvider.getIdentity(token);
+            log.info("identity: {}", identity);
+            CustomUserDetails userDetails = customUserDetailService.loadUserByUsername(identity);
+            log.info("userDetail.getAuthorities: {}", userDetails.getAuthorities());
 
-            // 추출된 JWT 토큰이 null이 아닌 경우, 해당 토큰에서 identity 값을 가져오기
-            if (token != null) {
-                String identity = jwtTokenProvider.getIdentity(token);
-                log.info("identity: {}", identity);
-                CustomUserDetails userDetails = customUserDetailService.loadUserByUsername(identity);
-                log.info("userDetail.getAuthorities: {}", userDetails.getAuthorities());
-
-                // 가져온 사용자 정보를 사용하여 UsernamePasswordAuthenticationToken 객체를 생성하고, SecurityContext에 이를 설정
-                Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-                SecurityContext context = SecurityContextHolder.getContext();
-                context.setAuthentication(authentication);
-                SecurityContextHolder.setContext(context);
-                log.info("SecurityContextHolder 저장 완료");
+            // 가져온 사용자 정보를 사용하여 UsernamePasswordAuthenticationToken 객체를 생성하고, SecurityContext에 이를 설정
+            Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+            SecurityContext context = SecurityContextHolder.getContext();
+            context.setAuthentication(authentication);
+            SecurityContextHolder.setContext(context);
+            log.info("SecurityContextHolder 저장 완료");
+            Long nationId = jwtTokenProvider.getNation(token);
+            if (role.equals(Role.STUDENT)) {
+                if (nationId != null) {
+                    String redirectUrl = "https://k8d103.p.ssafy.io/student/home";
+                    response.sendRedirect(redirectUrl);
+                    return;
+                }
+                else {
+                    String redirectUrl = "https://k8d103.p.ssafy.io/student/enter";
+                    response.sendRedirect(redirectUrl);
+                    return;
+                }
+            } else if (role.equals(Role.TEACHER)) {
+                if (nationId != null) {
+                    String redirectUrl = "https://k8d103.p.ssafy.io/teacher/class/students";
+                    response.sendRedirect(redirectUrl);
+                    return;
+                }
+                else {
+                    String redirectUrl = "https://k8d103.p.ssafy.io/teacher/create";
+                    response.sendRedirect(redirectUrl);
+                    return;
+                }
             }
-        } catch (ExpiredJwtException e){
-            log.info("[doFilterInternal]에서 발생 : {}", e.getMessage());
-            e.printStackTrace();
+            // HTTP 요청을 필터링한 후 다음 필터로 체인을 전달
+            filterChain.doFilter(request, response);
         }
-        // HTTP 요청을 필터링한 후 다음 필터로 체인을 전달
-        filterChain.doFilter(request, response);
+        else {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "토큰이 없습니다.");
+            // HTTP 요청을 필터링한 후 다음 필터로 체인을 전달
+            filterChain.doFilter(request, response);
+        }
     }
 }
