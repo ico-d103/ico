a6a9fbf48e3dae3bea2237fed963a965875d5e44
sagongjieun <gonii0821@naver.com>
2023-05-12 16:18:23 +0900

    feat: 학생 - 직업 목록 조회 기능 구현

--- ico/src/api/teacher/gov/getGovJobAPI.ts
+++ ico/src/api/teacher/gov/getGovJobAPI.ts
@@ -1,21 +1,18 @@
 import { tokenInstance } from "@/api/instance"
 import { getGovJobType } from "@/types/teacher/apiReturnTypes"
 
-type paramsType = {
-}
+type paramsType = {}
 
 type responseType = {
-    status: number
-    data: getGovJobType[]
+	status: number
+	data: getGovJobType[]
 }
 
 export const getGovJobAPI = async ({}: paramsType) => {
-    try {
-        const response: responseType = await tokenInstance.get(`/job`)
-        return response.data
-    } catch (error) {
-        throw error
-        console.log(error)
-    }
+	try {
+		const response: responseType = await tokenInstance.get(`/job`)
+		return response.data
+	} catch (error) {
+		throw error
+	}
 }
-
