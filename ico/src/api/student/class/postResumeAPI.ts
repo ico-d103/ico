9a3bc4a706208d6f0dfe92b6ece0055c29dca90d
sagongjieun <gonii0821@naver.com>
2023-05-12 10:47:59 +0900

    feat: 직업 신청 기능 api 생성

---
+++ ico/src/api/student/class/postResumeAPI.ts
@@ -0,0 +1,20 @@
+import { tokenInstance } from "@/api/instance"
+import { successReturnType } from "@/types/common/apiReturnTypes"
+
+type paramsType = {
+	id: number
+}
+
+type responseType = {
+	status: number
+	data: successReturnType
+}
+
+export const postResumeAPI = async ({ id }: paramsType) => {
+	try {
+		const response: responseType = await tokenInstance.post(`/resume/student/${id}`)
+		return response.data
+	} catch (error) {
+		throw error
+	}
+}
