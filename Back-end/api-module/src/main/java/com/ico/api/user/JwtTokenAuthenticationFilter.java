a1a27168619ed0e75c99fc0fce7b3dac39c4042e
강교철 <kys04171@naver.com>
2023-05-12 20:54:38 +0900

    feat : 전역으로 토큰이 없을 때 NOT_FOUND_TOKEN 커스텀 에러 return

--- Back-end/api-module/src/main/java/com/ico/api/user/JwtTokenAuthenticationFilter.java
+++ Back-end/api-module/src/main/java/com/ico/api/user/JwtTokenAuthenticationFilter.java
@@ -2,8 +2,6 @@ package com.ico.api.user;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ico.core.exception.ErrorResponse;
-import com.ico.core.exception.TokenNotFoundException;
-import io.jsonwebtoken.ExpiredJwtException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -43,34 +41,38 @@ public class JwtTokenAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
-        String token = "";
+        String token = jwtTokenProvider.parseJwt(request);
+        if (token == null || token.trim().isEmpty()) {
+            if (request.getRequestURI().startsWith("/api/login") || request.getRequestURI().startsWith("/api/student")
+                    || request.getRequestURI().startsWith("/api/teacher") || request.getRequestURI().startsWith("/api/duplicated-id")) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            response.setCharacterEncoding("UTF-8");
+            ErrorResponse errorResponse = new ErrorResponse("25", "토큰이 없습니다.");
+            objectMapper.writeValue(response.getWriter(), errorResponse);
+            return;
+        }
         try {
             // HttpServletRequest 객체에서 JWT 토큰을 추출
-            token = jwtTokenProvider.parseJwt(request);
             log.info("request: {}", request.getHeader("Authorization"));
             log.info("token: {}", token);
 
             // 추출된 JWT 토큰이 null이 아닌 경우, 해당 토큰에서 identity 값을 가져오기
-            if (token != null) {
-                String identity = jwtTokenProvider.getIdentity(token);
-                log.info("identity: {}", identity);
-                CustomUserDetails userDetails = customUserDetailService.loadUserByUsername(identity);
-                log.info("userDetail.getAuthorities: {}", userDetails.getAuthorities());
+            String identity = jwtTokenProvider.getIdentity(token);
+            log.info("identity: {}", identity);
+            CustomUserDetails userDetails = customUserDetailService.loadUserByUsername(identity);
+            log.info("userDetail.getAuthorities: {}", userDetails.getAuthorities());
 
-                // 가져온 사용자 정보를 사용하여 UsernamePasswordAuthenticationToken 객체를 생성하고, SecurityContext에 이를 설정
-                Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-                SecurityContext context = SecurityContextHolder.getContext();
-                context.setAuthentication(authentication);
-                SecurityContextHolder.setContext(context);
-                log.info("SecurityContextHolder 저장 완료");
-            } else {
-                // 로그인 전 일 경우에는 토큰이 없어도 요청 처리 가능
-                if (request.getRequestURI().startsWith("/api/login") || request.getRequestURI().startsWith("/api/student")
-                        || request.getRequestURI().startsWith("/api/teacher") || request.getRequestURI().startsWith("/api/duplicated-id")) {
-                    filterChain.doFilter(request, response);
-                    return;
-                }
-            }
+            // 가져온 사용자 정보를 사용하여 UsernamePasswordAuthenticationToken 객체를 생성하고, SecurityContext에 이를 설정
+            Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+            SecurityContext context = SecurityContextHolder.getContext();
+            context.setAuthentication(authentication);
+            SecurityContextHolder.setContext(context);
+            log.info("SecurityContextHolder 저장 완료");
+
         } catch (IllegalArgumentException ex) {
             response.setStatus(HttpStatus.UNAUTHORIZED.value());
             response.setContentType(MediaType.APPLICATION_JSON_VALUE);
