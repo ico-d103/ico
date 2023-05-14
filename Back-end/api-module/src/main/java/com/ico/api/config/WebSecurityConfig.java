7a8d47cfc4ea4fe5c5d4c6a051471ea421a35364
서재건 <sjk1052005@gmail.com>
2023-05-14 15:25:26 +0900

    fix : CORS 전체 허용 (CORS 에러 해결 전 임시 방편)

--- Back-end/api-module/src/main/java/com/ico/api/config/WebSecurityConfig.java
+++ Back-end/api-module/src/main/java/com/ico/api/config/WebSecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -71,6 +70,7 @@ public class WebSecurityConfig{
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList(
+                "*",
                 "http://localhost:3000",
                 "http://localhost:8081",
                 "https://k8d103.p.ssafy.io"
