748778f2bf27ab63a5e1e06f950e97c9a8521a57
강교철 <kys04171@naver.com>
2023-05-15 10:52:07 +0900

    edit : 예외처리 변경

--- Back-end/api-module/src/main/java/com/ico/api/user/JwtTokenAuthenticationFilter.java
+++ Back-end/api-module/src/main/java/com/ico/api/user/JwtTokenAuthenticationFilter.java
@@ -42,8 +42,9 @@ public class JwtTokenAuthenticationFilter extends OncePerRequestFilter {
         // token이 없을 때
         if (token == null || token.trim().isEmpty()) {
             // 토큰이 없더라도 요청가능한 api uri(/api/teacher/** != /api/teacher)
-            if (request.getRequestURI().startsWith("/api/login") || request.getRequestURI().startsWith("/api/student")
-                    || request.getRequestURI().startsWith("/api/teacher") || request.getRequestURI().startsWith("/api/duplicated-id")) {
+            if (request.getRequestURI().equals("/api/login") || request.getRequestURI().equals("/api/student")
+                    || request.getRequestURI().equals("/api/teacher") || request.getRequestURI().equals("/api/duplicated-id")
+                    || request.getRequestURI().equals("/api/teacher/phone")) {
                 log.info("[doFilterInternal] : 토큰이 없는 uri : {}", request.getRequestURI());
                 response.addHeader("Access-Control-Allow-Origin", "*");
                 filterChain.doFilter(request, response);
