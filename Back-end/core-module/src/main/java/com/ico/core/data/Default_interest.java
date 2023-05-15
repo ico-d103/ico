2286e1bcdf49b5f7d5bf171e1f13f784177d0ad8
서재건 <sjk1052005@gmail.com>
2023-05-15 19:45:43 +0900

    docs : javadoc 추가

--- Back-end/core-module/src/main/java/com/ico/core/data/Default_interest.java
+++ Back-end/core-module/src/main/java/com/ico/core/data/Default_interest.java
@@ -3,6 +3,11 @@ package com.ico.core.data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+/**
+ * 나라의 기본 예금 이자율
+ *
+ * @author 서재건
+ */
 @Getter
 @NoArgsConstructor
 public class Default_interest {
--- Back-end/core-module/src/main/java/com/ico/core/data/Default_rule.java
+++ Back-end/core-module/src/main/java/com/ico/core/data/Default_rule.java
@@ -3,6 +3,11 @@ package com.ico.core.data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+/**
+ * 나라의 기본 학급규칙
+ *
+ * @author 서재건
+ */
 @Getter
 @NoArgsConstructor
 public class Default_rule {
