324af40c13b69a722ea0ed63a8e98751ed022633
DongJu <jook1356@gmail.com>
2023-05-17 21:56:21 +0900

    chore: https 리다이렉트 테스트

--- ico/src/pages/_middleware.ts
+++ ico/src/pages/_middleware.ts
@@ -1,3 +1,15 @@
-import sslRedirect from 'next-ssl-redirect-middleware';
+import  { NextFetchEvent, NextRequest, NextResponse } from 'next/server'
 
-export default sslRedirect({});
+type Environment = "production" | "development" | "other";
+export function middleware(req: NextRequest, ev: NextFetchEvent) {
+    const currentEnv = process.env.NODE_ENV as Environment;
+
+    if (currentEnv === 'production' && 
+        req.headers.get("x-forwarded-proto") !== "https") {
+        return NextResponse.redirect(
+           `https://${req.headers.get('host')}${req.nextUrl.pathname}`,
+           301
+        );
+    } 
+    return NextResponse.next();
+}
