ec7990141414b8a049e1f84811bc357b0cfe3365
강교철 <kys04171@naver.com>
2023-05-18 15:02:19 +0900

    edit : 학생 생성시 1000원 기본 지급

--- Back-end/api-module/src/main/java/com/ico/api/service/student/StudentServiceImpl.java
+++ Back-end/api-module/src/main/java/com/ico/api/service/student/StudentServiceImpl.java
@@ -80,7 +80,7 @@ public class StudentServiceImpl implements StudentService{
                 .identity(requestDto.getIdentity())
                 .password(requestDto.getPassword())
                 .name(requestDto.getName())
-                .account(0)
+                .account(1000)
                 .isFrozen(false)
                 .creditScore((short) 700)
                 .creditRating((byte) 6)
