43640c78856ce604313d947f2fdcf188e7531987
Dongju Kim <jook1356@gmail.com>
2023-05-18 15:40:20 +0900

    chore: https 리다이렉트 테스트

--- ico/src/pages/middleware.ts
+++ ico/src/pages/middleware.ts
@@ -1,9 +1,15 @@
-export const middleware = (req: any, res: any, next: any) => {
-    const host = req.get("host");
-    const protocol = req.headers["x-forwarded-proto"] || req.protocol;
-    if (host === "k8d103.p.ssafy.io" || protocol !== "https") {
-      res.redirect(301, `https://k8d103.p.ssafy.io${req.url}`);
-    } else {
-      next();
-    }
-  };
+import  { NextFetchEvent, NextRequest, NextResponse } from 'next/server'
+
+type Environment = "production" | "development" | "other";
+export function middleware(req: NextRequest, ev: NextFetchEvent) {
+    const currentEnv = process.env.NODE_ENV as Environment;
+
+    if (currentEnv === 'production' && 
+        req.headers.get("x-forwarded-proto") !== "https") {
+        return NextResponse.redirect(
+           `https://${req.nextUrl.host}${req.nextUrl.pathname}`,
+           301
+        );
+    } 
+    return NextResponse.next();
+}
