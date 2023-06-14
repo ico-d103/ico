45604ebc298aa5101e240a29b183c233256b0705
사공지은 <gonii0821@naver.com>
2023-06-15 02:20:39 +0900

    feat: 학생 자격증 UI 구현중

--- ico/src/components/teacher/Class/Student/Detail/ClassStudentDetailCertificate.tsx
+++ ico/src/components/teacher/Class/Student/Detail/ClassStudentDetailCertificate.tsx
@@ -1,15 +1,81 @@
 import { licenseType } from "@/types/teacher/apiReturnTypes"
 import { css } from "@emotion/react"
+import ClassStudentDetailCertificateItem from "./ClassStudentDetailCertificateItem"
 
 type ClassStudentDetailCertificatePropsType = {
 	license: licenseType[] | undefined
 }
 
 function ClassStudentDetailCertificate({ license }: ClassStudentDetailCertificatePropsType) {
+	const mockList = [
+		{
+			id: 840,
+			subject: "수학",
+			rating: 1,
+		},
+		{
+			id: 841,
+			subject: "과학",
+			rating: 1,
+		},
+		{
+			id: 842,
+			subject: "사회",
+			rating: 0,
+		},
+		{
+			id: 843,
+			subject: "독서",
+			rating: -1,
+		},
+		{
+			id: 844,
+			subject: "바른 글씨",
+			rating: -1,
+		},
+		{
+			id: 845,
+			subject: "정리 정돈",
+			rating: 7,
+		},
+		{
+			id: 846,
+			subject: "체력",
+			rating: -1,
+		},
+		{
+			id: 847,
+			subject: "디자인",
+			rating: -1,
+		},
+		{
+			id: 848,
+			subject: "저축",
+			rating: -1,
+		},
+		{
+			id: 849,
+			subject: "운전면허",
+			rating: -1,
+		},
+	]
+
 	return (
 		<div css={wrapperCSS}>
 			<h4>자격증</h4>
-			<div css={noneWrapperCSS}>{license?.length ? <h5>자격증이 있습니다.</h5> : <h5>자격증이 없습니다.</h5>}</div>
+			<div css={contentCSS}>
+				{mockList?.length ? (
+					<div css={listWrapperCSS}>
+						{mockList.map((mock) => (
+							<ClassStudentDetailCertificateItem key={mock.id} certificate={mock} />
+						))}
+					</div>
+				) : (
+					<div css={noneListWrapperCSS}>
+						<h5>자격증이 없습니다.</h5>
+					</div>
+				)}
+			</div>
 		</div>
 	)
 }
@@ -30,7 +96,18 @@ const wrapperCSS = css`
 	}
 `
 
-const noneWrapperCSS = css`
+const contentCSS = css`
+	height: 200px;
+	overflow-y: scroll;
+`
+
+const listWrapperCSS = css`
+	display: flex;
+	flex-direction: column;
+	gap: 15px;
+`
+
+const noneListWrapperCSS = css`
 	display: flex;
 	justify-content: center;
 	align-items: center;
---
+++ ico/src/components/teacher/Class/Student/Detail/ClassStudentDetailCertificateRange.tsx
@@ -0,0 +1,13 @@
+import { css } from "@emotion/react"
+
+type ClassStudentDetailCertificateRangePropsType = {
+	range: number
+}
+
+function ClassStudentDetailCertificateRange({ range }: ClassStudentDetailCertificateRangePropsType) {
+	return <div css={wrapperCSS}></div>
+}
+
+const wrapperCSS = css``
+
+export default ClassStudentDetailCertificateRange
