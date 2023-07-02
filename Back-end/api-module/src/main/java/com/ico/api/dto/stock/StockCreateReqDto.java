b68927178274393b3a65d16f14838a52a7de3897
forever4410 <forever4410@gmail.com>
2023-07-03 01:03:46 +0900

    edit : PR Comment 반영

--- Back-end/api-module/src/main/java/com/ico/api/dto/stock/StockCreateReqDto.java
+++ Back-end/api-module/src/main/java/com/ico/api/dto/stock/StockCreateReqDto.java
@@ -4,6 +4,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
@@ -23,14 +24,15 @@ public class StockCreateReqDto {
 
     // 주식 시작 가격
     @NotNull(message = "711")
-    private double amount;
+    @Min(0)
+    private Double amount;
 
     // 내일의 첫 이슈 등록
     @NotBlank(message = "713")
     private String issue;
 
     @Builder
-    public StockCreateReqDto(String title, String content, double amount, String issue) {
+    public StockCreateReqDto(String title, String content, Double amount, String issue) {
         this.title = title;
         this.content = content;
         this.amount = amount;
