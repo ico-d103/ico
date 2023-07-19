2ff8940eac71bb377088e3471e312cfbecf02d8a
사공지은 <gonii0821@naver.com>
2023-07-19 23:24:47 +0900

    feat: 기업 추가 폼 수정

--- ico/src/components/teacher/Gov/Corporate/Create/GovCorporateCreateContents.tsx
+++ ico/src/components/teacher/Gov/Corporate/Create/GovCorporateCreateContents.tsx
@@ -5,7 +5,21 @@ import GovCorporateCreateContent from "./GovCorporateCreateContent"
 function GovCorporateCreateContents() {
 	return (
 		<div css={wrapperCSS}>
-			<div>
+			<div css={rowCSS}>
+				<GovCorporateCreateContent
+					label={"업종"}
+					essential={true}
+					placeholder={"업종을 최대 10자 이내로 입력해주세요."}
+					type={"corporate_type"}
+				/>
+				<GovCorporateCreateContent
+					label={"등록 번호"}
+					essential={true}
+					placeholder={"기업의 고유한 등록 번호를 입력해주세요."}
+					type={"corporate_number"}
+				/>
+			</div>
+			<div css={rowCSS}>
 				<GovCorporateCreateContent
 					label={"기업명"}
 					essential={true}
@@ -13,17 +27,13 @@ function GovCorporateCreateContents() {
 					type={"corporate_name"}
 				/>
 				<GovCorporateCreateContent
-					label={"기업 설명"}
-					essential={true}
-					placeholder={"기업에 대한 설명을 200자 이내로 입력해주세요."}
-					type={"corporate_desc"}
-				/>
-				<GovCorporateCreateContent
 					label={"기업 로고"}
 					essential={true}
 					placeholder={"기업 로고 사진을 등록해주세요."}
 					type={"corporate_logo"}
 				/>
+			</div>
+			<div css={rowCSS}>
 				<GovCorporateCreateContent
 					label={"CEO"}
 					essential={true}
@@ -37,19 +47,32 @@ function GovCorporateCreateContents() {
 					type={"corporate_capital"}
 				/>
 			</div>
+			<GovCorporateCreateContent
+				label={"기업 설명"}
+				essential={true}
+				placeholder={"기업에 대한 설명을 200자 이내로 입력해주세요."}
+				type={"corporate_desc"}
+				isTextArea={true}
+			/>
 		</div>
 	)
 }
 
 const wrapperCSS = css`
+	width: 90%;
 	display: flex;
 	flex-direction: column;
+	gap: 40px;
+`
+
+const rowCSS = css`
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	gap: 50px;
 
 	> div {
 		width: 90%;
-		display: flex;
-		flex-direction: column;
-		gap: 20px;
 	}
 `
 
