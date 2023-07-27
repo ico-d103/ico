c858ec53390dad81e9235638397688b72a6b3089
강교철 <kys04171@naver.com>
2023-07-27 19:26:01 +0900

    docs : docs 추가

--- Back-end/api-module/src/main/java/com/ico/api/dto/license/StudentLicenseReqDto.java
+++ Back-end/api-module/src/main/java/com/ico/api/dto/license/StudentLicenseReqDto.java
@@ -6,6 +6,11 @@ import lombok.NoArgsConstructor;
 import javax.validation.constraints.NotNull;
 import java.util.Map;
 
+/**
+ * 학생 자격증 등급 조정시에 받는 Dto
+ *
+ * @author 강교철
+ */
 @Getter
 @NoArgsConstructor
 public class StudentLicenseReqDto {
