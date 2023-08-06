cf351c3d9f9e5f8c4d05e6cc2961ce4af7a50a61
서재건 <sjk1052005@gmail.com>
2023-08-06 23:14:16 +0900

    edit : Valid, RequestBody annotation 추가

--- Back-end/api-module/src/main/java/com/ico/api/controller/SalaryController.java
+++ Back-end/api-module/src/main/java/com/ico/api/controller/SalaryController.java
@@ -6,11 +6,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 
 /**
@@ -33,8 +35,8 @@ public class SalaryController {
      * @param request
      * @return
      */
-    @GetMapping("/teacher/payment")
-    public ResponseEntity<HttpStatus> paySalary(SalaryPaymentReqDto dto, HttpServletRequest request) {
+    @PostMapping("/teacher/payment")
+    public ResponseEntity<HttpStatus> paySalary(@Valid @RequestBody SalaryPaymentReqDto dto, HttpServletRequest request) {
         salaryService.paySalary(dto, request);
         return ResponseEntity.ok(HttpStatus.OK);
     }
