ca80ea658d83b4d1b29df515d265f62813946000
DESKTOP-1JVI7QI\SSAFY <sjk1052005@gmail.com>
2023-08-07 16:58:04 +0900

    chore : 사용하지 않는 test 코드 주석 처리

--- Back-end/batch-module/src/test/java/com/ico/batch/BatchModuleApplicationTests.java
+++ Back-end/batch-module/src/test/java/com/ico/batch/BatchModuleApplicationTests.java
@@ -1,13 +1,13 @@
-package com.ico.batch;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class BatchModuleApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.ico.batch;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class BatchModuleApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
