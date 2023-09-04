c7bdcb38809f8303c91696748c005ee6b28aa54b
서재건 <sjk1052005@gmail.com>
2023-09-04 21:01:55 +0900

    chore : 규칙 -> 소식으로 변경

--- Back-end/core-module/src/main/java/com/ico/core/repository/NewsRepository.java
+++ Back-end/core-module/src/main/java/com/ico/core/repository/NewsRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 /**
- * 학급 규칙 관련 repository
+ * 학급 소식 관련 repository
  *
  * @author 서재건
  */
@@ -18,7 +18,7 @@ public interface NewsRepository extends JpaRepository<News, Long> {
     List<News> findAllByNationId(Long nationId);
 
     /**
-     * 전체 학급 규칙 조회
+     * 전체 학급 소식 조회
      *
      * @param nationId
      * @return
@@ -26,7 +26,7 @@ public interface NewsRepository extends JpaRepository<News, Long> {
     List<News> findAllByNationIdOrderByUpdatedAtDesc(Long nationId);
 
     /**
-     * 학급 규칙 생성 시 제목 중복 체크
+     * 학급 소식 생성 시 제목 중복 체크
      *
      * @param nationId
      * @param title
@@ -35,12 +35,12 @@ public interface NewsRepository extends JpaRepository<News, Long> {
     Optional<News> findByNationIdAndTitle(Long nationId, String title);
 
     /**
-     * 학급 규칙 수정 시 제목 중복 제크
+     * 학급 소식 수정 시 제목 중복 제크
      *
-     * @param ruleId
+     * @param newsId
      * @param nationId
      * @param title
      * @return
      */
-    Optional<News> findByIdNotAndNationIdAndTitle(Long ruleId, Long nationId, String title);
+    Optional<News> findByIdNotAndNationIdAndTitle(Long newsId, Long nationId, String title);
 }
