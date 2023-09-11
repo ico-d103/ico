51e7ae2cfb225bda579dc98e8e1d85202cb116b5
서재건 <sjk1052005@gmail.com>
2023-09-12 00:09:09 +0900

    chore : QR 상품 구매 request(ProductQRReqDto)에서 사용하는 변수명 변경
    - productList -> products

--- Back-end/api-module/src/main/java/com/ico/api/dto/teacherProduct/ProductQRReqDto.java
+++ Back-end/api-module/src/main/java/com/ico/api/dto/teacherProduct/ProductQRReqDto.java
@@ -10,7 +10,7 @@ import javax.validation.constraints.NotNull;
 import java.util.List;
 
 /**
- * QR 상품 구매 dto
+ * QR 상품 구매 request dto
  *
  * @author 변윤경
  * @author 서재건
@@ -21,14 +21,14 @@ import java.util.List;
 public class ProductQRReqDto {
 
     @NotEmpty(message = "622")
-    List<ProductQRColDto> productList;
+    List<ProductQRColDto> products;
 
     @NotNull(message = "619")
     private Long unixTime;
 
     @Builder
-    public ProductQRReqDto(List<ProductQRColDto> productList, Long unixTime) {
-        this.productList = productList;
+    public ProductQRReqDto(List<ProductQRColDto> products, Long unixTime) {
+        this.products = products;
         this.unixTime = unixTime;
     }
 }
