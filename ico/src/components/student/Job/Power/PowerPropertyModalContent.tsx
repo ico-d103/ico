32687b7bfa8d1d20c9457ee3ac08a6567157b28c
사공지은 <gonii0821@naver.com>
2023-09-18 20:26:13 +0900

    feat: 권한 있는 학생의 국세 조정 기능 구현

--- ico/src/components/student/Job/Power/PowerPropertyModalContent.tsx
+++ ico/src/components/student/Job/Power/PowerPropertyModalContent.tsx
@@ -1,20 +1,95 @@
+import { postTreasuryAPI } from "@/api/teacher/class/postTreasuryAPI"
 import Button from "@/components/common/Button/Button"
+import NotiTemplate from "@/components/common/StackNotification/NotiTemplate"
+import useNotification from "@/hooks/useNotification"
+import { NUM_ONLY } from "@/util/regex"
 import { css } from "@emotion/react"
+import { useQueryClient } from "@tanstack/react-query"
+import { useReducer } from "react"
 
 function PowerPropertyModalContent() {
+	const noti = useNotification()
+	const queryClient = useQueryClient()
+	const inputReducer = (
+		state: { title: string; source: string; amount: string },
+		action: { type: string; value: string },
+	) => {
+		switch (action.type) {
+			case "CHANGE_AMOUNT":
+				return { ...state, amount: action.value }
+			case "CHANGE_TITLE":
+				return { ...state, title: action.value }
+			case "CHANGE_SOURCE":
+				return { ...state, source: action.value }
+			default:
+				return state
+		}
+	}
+	const [inputState, dispatchInput] = useReducer(inputReducer, { title: "", source: "", amount: "" })
+
+	const changeAmountHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+		let inputValue = e.target.value
+
+		if (!NUM_ONLY.test(inputValue)) {
+			inputValue = inputValue.replace(/\D/g, "")
+			e.target.value = inputValue
+			return
+		}
+
+		dispatchInput({ type: "CHANGE_AMOUNT", value: e.target.value })
+	}
+
+	const postTreasuryHandler = async (type: boolean) => {
+		if (inputState.title === "" || inputState.amount === "" || inputState.source === "") {
+			alert("빈칸을 모두 입력해주세요.")
+			return
+		}
+
+		const amount = type ? inputState.amount : "-" + inputState.amount
+		const numberAmount = Number(amount)
+
+		const response = await postTreasuryAPI({
+			body: { title: inputState.title, source: inputState.source, amount: numberAmount },
+		})
+
+		if (response === "OK") {
+			dispatchInput({ type: "CHANGE_AMOUNT", value: "" })
+			dispatchInput({ type: "CHANGE_SOURCE", value: "" })
+			dispatchInput({ type: "CHANGE_TITLE", value: "" })
+
+			noti({
+				content: <NotiTemplate type={"ok"} content={type ? "입금되었습니다." : "출금되었습니다."} />,
+				duration: 3000,
+			})
+		} else {
+			noti({
+				content: <NotiTemplate type={"alert"} content={`오류가 발생했습니다. 다시 시도해주세요.`} />,
+				duration: 3000,
+			})
+		}
+	}
+
 	return (
-		<div>
+		<div css={wrapperCSS} onClick={(e) => e.stopPropagation()}>
 			<div css={inputWrapperCSS}>
 				<div>금액을 입력해주세요.</div>
-				<input placeholder="ex) 2000" />
+				<input placeholder="ex) 2000" onChange={changeAmountHandler} value={inputState.amount} />
 			</div>
 			<div css={inputWrapperCSS}>
 				<div>사용 종류를 입력해주세요.</div>
-				<input placeholder="ex) 세금" />
+				<input
+					placeholder="ex) 세금"
+					onChange={(e) => dispatchInput({ type: "CHANGE_SOURCE", value: e.target.value })}
+					value={inputState.source}
+				/>
 			</div>
 			<div css={inputWrapperCSS}>
 				<div>사유를 입력해주세요.</div>
-				<input placeholder="ex) 전기세, 자리세" />
+				<input
+					placeholder="ex) 전기세, 자리세"
+					onChange={(e) => dispatchInput({ type: "CHANGE_TITLE", value: e.target.value })}
+					value={inputState.title}
+				/>
 			</div>
 			<div css={buttonWrapperCSS}>
 				<Button
@@ -22,19 +97,30 @@ function PowerPropertyModalContent() {
 					fontSize={"var(--teacher-h5)"}
 					width={"110px"}
 					theme={"manageMinus"}
-					onClick={() => {}}
+					onClick={() => postTreasuryHandler(false)}
 				/>
 				<Button
 					text={"입금하기"}
 					fontSize={"var(--teacher-h5)"}
 					width={"110px"}
 					theme={"mobileSoft3"}
-					onClick={() => {}}
+					onClick={() => postTreasuryHandler(true)}
 				/>
 			</div>
 		</div>
 	)
 }
+
+const wrapperCSS = css`
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+
+	> div {
+		width: 65%;
+	}
+`
 
 const inputWrapperCSS = css`
 	display: flex;
@@ -54,8 +140,9 @@ const inputWrapperCSS = css`
 const buttonWrapperCSS = css`
 	display: flex;
 	flex-direction: row;
+	justify-content: center;
 	gap: 20px;
-	margin: 20px 0;
+	margin-bottom: 20px;
 `
 
 export default PowerPropertyModalContent
